fb1662e1b439f4ac20aafed773d7b51fbac6d234
Your Name <devanshgupta9725@gmail.com>
2026-03-30 00:23:10 +0530

    removal of unnecessary files

--- test.py
+++
@@ -1,6 +0,0 @@
-from backend.api import resolve_player_name, map_engine
-import json
-r = resolve_player_name('f0rsaken') or 'f0rsaken'
-score = map_engine.get_player_map_score(r, 'Lotus')
-with open('out.txt', 'w', encoding='utf-8') as f:
-    f.write(f"Resolved: {repr(r)}\nScore: {json.dumps(score)}\n")
